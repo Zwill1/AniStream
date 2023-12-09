2f8e0a68feefbd9e38c0bd358b75d197fcdda6ee
zwill <zwill_1@hotmail.com>
2023-12-08 23:25:42 -0700

    adding context to app

--- src/app/ui/LoginForm.tsx
+++ src/app/ui/LoginForm.tsx
@@ -1,4 +1,7 @@
 export default function LoginForm() {
+
+  
+
   return (
     <>
       <section className="bg-gray-50 dark:bg-gray-900">
@@ -28,8 +31,8 @@ export default function LoginForm() {
                             </div>
                             <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                         </div>
-                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                        <button type="submit" className="w-full text-white bg-black hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                        <p className="text-sm font-light text-gray-500 dark:text-gray-400 text-center">
                             Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                         </p>
                     </form>
