f62535619623b8d3f343550d5f741020ce431fa3
zwill <zwill_1@hotmail.com>
2023-12-25 19:46:02 -0700

    setting up profile pages per id

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -9,14 +9,27 @@ export default async function AnimeProfile({ params }: { params: { id: string } 
 
   const data = await fetchAnimeById(newUrlId);
 
+  console.log(data);
+
   return (
     <>
-      <div className="sm:p-16 py-16 px-8 flex flex-col gap-10">
-        {/* <h2 className="text-3xl text-white font-bold">Profile page id : {params.id} / {newUrlId}</h2> */}
+      <div className="grid grid-cols-6">
+          <div className="p-4">
+            {data}
+          </div>
+          <div className="grid grid-cols-subgrid gap-10 col-span-5">
+            <div className="col-start-1 p-4">
+              <h2 className="text-2xl font-extrabold">{data.props.anime} / <span className="text-gray-500 italic text-sm">{data.props.engName}</span></h2>
+              <p>desc</p>
+            </div>
+          </div>
+      </div>
+      {/* <div className="sm:p-16 py-16 px-8 flex flex-row gap-10">
+        <h2 className="text-3xl text-white font-bold">Profile page id : {params.id} / {newUrlId}</h2>
         <section className="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
           {data}
         </section>
-      </div>
+      </div> */}
     </>
   )
 }
