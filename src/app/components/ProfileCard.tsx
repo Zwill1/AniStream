b4ba7870858e63a9d5ef56fc8cfa852e28f14d01
zwill <zwill_1@hotmail.com>
2023-12-27 01:09:04 -0700

    adding screenshots and video to page

--- src/app/components/ProfileCard.tsx
+++ src/app/components/ProfileCard.tsx
@@ -14,6 +14,13 @@ export interface AnimeProfileCard {
   description: string;
   aired_on: string;
   status: string;
+  screenshots: {
+    preview: string
+  }
+  videos: {
+    url: string
+  }
+  rating: string;
 }
 
 interface PropProfile {
@@ -32,7 +39,7 @@ function ProfileCard({ anime } : PropProfile) {
       <div className="max-w-sm rounded relative w-full">
         <div className="relative w-full h-[37vh]">
           <Image
-            src={`https://shikimori.one${anime.image.original}}`}
+            src={`https://shikimori.one${anime.image.original}`}
             alt={anime.name}
             fill
             className="rounded-xl"
@@ -42,7 +49,7 @@ function ProfileCard({ anime } : PropProfile) {
       <div className="grid grid-cols-subgrid gap-10 col-span-5">
         <div className="col-start-1 px-10">
           <h2 className="text-3xl text-white font-extrabold">{anime.name} / <span className="text-gray-500 italic text-sm">{anime.english}</span></h2>
-          <div className="flex flex-row w-full">
+          <div className="flex flex-row w-full my-3">            
             <div className="w-3/12 flex flex-row gap-2 items-center">
               <Image
                 src="../star.svg"
@@ -53,50 +60,60 @@ function ProfileCard({ anime } : PropProfile) {
               />
               <p className="text-base font-bold text-[#FFAD49]">Score: {anime.score}</p>
             </div>
-            <p className="w-3/12">Episode: {anime.episodes}</p>
+
+            <div className="w-3/12 flex flex-row gap-2 items-center">
+              <Image
+                src="../episodes.svg"
+                alt="episodes"
+                width={20}
+                height={20}
+                className="object-contain"
+              />
+              <p className="text-base text-white font-bold">Episode: {anime.episodes}</p>
+            </div>
+
+            
             <p className="w-3/12">Aired On: {anime.aired_on}</p>
+            <p className="w-3/12">Rating: {anime.rating}</p>
             <p className="w-3/12">Status: {anime.status}</p>
           </div>
           <p>{anime.description}</p>
         </div>
       </div>
+    </div>
 
-      {/* <div className="py-4 flex flex-col gap-3">
-        <div className="flex justify-between items-center gap-1">
-          <h2 className="font-bold text-white text-xl line-clamp-1 w-full">
-            {anime.name}
-          </h2>
-          <div className="py-1 px-2 bg-[#161921] rounded-sm">
-            <p className="text-white text-sm font-bold capitalize">
-              {anime.kind}
-            </p>
-          </div>
-        </div>
-        <div className="flex gap-4 items-center">
-          <div className="flex flex-row gap-2 items-center">
-            <Image
-              src="./episodes.svg"
-              alt="episodes"
-              width={20}
-              height={20}
-              className="object-contain"
-            />
-            <p className="text-base text-white font-bold">
-              {anime.episodes || anime.episodes_aired}
-            </p>
-          </div>
-          <div className="flex flex-row gap-2 items-center">
-            <Image
-              src="./star.svg"
-              alt="star"
-              width={18}
-              height={18}
-              className="object-contain"
-            />
-            <p className="text-base font-bold text-[#FFAD49]">{anime.score}</p>
-          </div>
-        </div>
-      </div> */}
+    <div className="flex flex-row w-full pl-4">
+      <h2 className="text-3xl text-white font-extrabold">Videos</h2>
+    </div>
+    <div className="flex flex-row w-full p-4 gap-4">
+      <div className="w-3/12 bg-red-100">
+        <video autoPlay style={{ width: '500px', height: '300px' }}>
+          <source src={anime.videos[0].url} />
+        </video>
+        {/* <p>s / {anime.videos[0].url}</p> */}
+      </div>
+    </div>
+
+    <div className="flex flex-row w-full pl-4">
+      <h2 className="text-3xl text-white font-extrabold">Screenshots</h2>
+    </div>
+    <div className="flex flex-row w-full p-4 gap-4">
+      <div className="w-3/12">
+        <Image
+          src={`https://shikimori.one${anime.screenshots[0].preview}`}
+          alt={anime.name}
+          width={500}
+          height={500}
+        />
+      </div>
+      <div className="w-3/12">
+        <Image
+          src={`https://shikimori.one${anime.screenshots[1].preview}`}
+          alt={anime.name}
+          width={500}
+          height={500}
+        />
+      </div>
     </div>
 
   </>
