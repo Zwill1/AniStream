10f31cb5cbfab048606d4972d707e4d5085e0b24
zwill <zwill_1@hotmail.com>
2024-01-14 02:13:29 -0700

    setting up data files for profile section

--- src/app/components/Profile/AnimeStats.tsx
+++ src/app/components/Profile/AnimeStats.tsx
@@ -1,4 +1,29 @@
+"use client";
+import { useState, useEffect } from 'react'
+import * as dataObj from "../../data/animeStatsData.json"
 export default function AnimeStats() {
+
+    // bring in a local json file with all the stats for anime being watched
+    
+    // const [data, setData] = useState(null)
+    // const [isLoading, setLoading] = useState(true)
+   
+    // useEffect(() => {
+    //   fetch('./src/app/data/animeStatsData.ts')
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       setData(data)
+    //       console.log(data);
+    //       setLoading(false)
+    //     })
+    // }, [])
+   
+    // if (isLoading) return <p>Loading...</p>
+    // if (!data) return <p>No Anime data</p>
+
+    const data = dataObj[0];
+    console.log(data.Watching);
+
   return (
     <>
     <div className="w-full">
@@ -7,11 +32,11 @@ export default function AnimeStats() {
         <div className="w-full lg:px-4 lg:flex lg:py-1">
             <div className="flex lg:w-6/12 justify-between px-2 lg:px-10">
                 <p>Watching</p>
-                <p>(number)</p>
+                <p>{data.Watching}</p>
             </div>
             <div className="flex lg:w-6/12 justify-between px-2 lg:px-10">
                 <p>Total Entries</p>
-                <p>(number)</p>
+                <p>{data.TotalEntries}</p>
             </div>
         </div>
 
@@ -19,11 +44,11 @@ export default function AnimeStats() {
         <div className="w-full lg:px-4 lg:flex lg:py-1">
             <div className="flex lg:w-6/12 justify-between px-2 lg:px-10">
                 <p>Completed</p>
-                <p>(number)</p>
+                <p>{data.Completed}</p>
             </div>
             <div className="flex lg:w-6/12 justify-between px-2 lg:px-10">
                 <p>Rewatched</p>
-                <p>(number)</p>
+                <p>{data.Rewatched}</p>
             </div>
         </div>
 
@@ -31,11 +56,11 @@ export default function AnimeStats() {
         <div className="w-full lg:px-4 lg:flex lg:py-1">
             <div className="flex lg:w-6/12 justify-between px-2 lg:px-10">
                 <p>On-Hold</p>
-                <p>(number)</p>
+                <p>{data.OnHold}</p>
             </div>
             <div className="flex lg:w-6/12 justify-between px-2 lg:px-10">
                 <p>Episode</p>
-                <p>(number)</p>
+                <p>{data.Episodes}</p>
             </div>
         </div>
 
@@ -43,11 +68,11 @@ export default function AnimeStats() {
         <div className="w-full lg:px-4 lg:flex lg:py-1">
             <div className="flex lg:w-6/12 justify-between px-2 lg:px-10">
                 <p>Dropped</p>
-                <p>(number)</p>
+                <p>{data.Dropped}</p>
             </div>
             <div className="flex lg:w-6/12 justify-between px-2 lg:px-10">
                 <p>Plan to Watch</p>
-                <p>(number)</p>
+                <p>{data.PlanToWatch}</p>
             </div>
         </div>
 
