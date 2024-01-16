709875225d3666501cfd6905e9b0763ed2ca6477
zwill <zwill_1@hotmail.com>
2024-01-16 00:53:24 -0700

    setting up data file for anime history

--- src/app/components/Profile/AnimeUpdates.tsx
+++ src/app/components/Profile/AnimeUpdates.tsx
@@ -1,9 +1,26 @@
+import Image from "next/image";
+
 export default function AnimeUpdates() {
   return (
     <>
         <div className="w-full">
-            {/* bring in a card map of a json file */}
-            <p>sd</p>
+          {/* bring in a card map list of a json file */}
+          {/* Items-center to vertical align */}
+          
+          <div className="flex">
+            <div className="flex-1 md:flex-2">
+              <Image src={`https://i.pravatar.cc/100?u=zz`} width={100} height={100} alt="Image" className="max-w-full mb-4" />
+            </div>
+            <div className="flex-1 md:flex-2">
+              <div>
+                <p className="text-center">(Anime Name)</p>
+                <p className="text-center">Watching (Number)/(Number)</p>
+              </div>
+            </div>
+            <div className="flex-1 md:flex-auto">
+              <p className="text-center">Time</p>
+            </div>
+          </div>
         </div>
     </>
   )
--- src/app/components/Profile/AnimeUpdatesHeading.tsx
+++ src/app/components/Profile/AnimeUpdatesHeading.tsx
@@ -6,8 +6,8 @@ export default function AnimeUpdatesHeading() {
     <>
         <div className="lg:w-6/12 w-full px-2">
             <div className="flex justify-between py-2">
-            <p className="font-bold italic text-red-500">Last Anime Updates</p>
-            <p><Link href="#" className="text-blue-500">Anime History</Link> </p>
+              <p className="font-bold italic text-red-500">Last Anime Updates</p>
+              <p><Link href="#" className="text-blue-500">Anime History</Link></p>
             </div>
             <AnimeUpdates />
         </div>
