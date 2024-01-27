7052d104690db2e0e8e4cb88801e2ac291eca7f5
zwill <zwill_1@hotmail.com>
2024-01-26 22:53:13 -0700

    setting up footer

--- src/app/ui/Footer.tsx
+++ src/app/ui/Footer.tsx
@@ -1,7 +1,26 @@
 export default function Footer() {
   return (
     <>
-        <div className="text-white">Footer</div>
+    <section className="pt-5">
+    <div className="lg:flex">
+      <div className="lg:w-3/4 text-white text-center">
+        <h4 className="text-sm font-bold mb-2">Footer</h4>
+        <p className="text-xs">text</p>
+      </div>
+      <div className="lg:w-3/4 text-white text-center">
+        <h4 className="text-sm font-bold mb-2">Footer</h4>
+        <p className="text-xs">text</p>
+      </div>
+      <div className="lg:w-3/4 text-white text-center">
+        <h4 className="text-sm font-bold mb-2">Footer</h4>
+        <p className="text-xs">text</p>
+      </div>
+      <div className="lg:w-3/4 text-white text-center">
+        <h4 className="text-sm font-bold mb-2">Footer</h4>
+        <p className="text-xs">text</p>
+      </div>
+    </div>
+    </section>
     </>
   )
 }
