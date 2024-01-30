c9af0dfce58898bcf9bd08d4c9e16e155d02297c
zwill <zwill_1@hotmail.com>
2024-01-29 23:49:36 -0700

    working on footer

--- src/app/ui/Footer.tsx
+++ src/app/ui/Footer.tsx
@@ -6,12 +6,17 @@ export default function Footer() {
     <section className="pt-5 w-full bg-[#0F1117]">
     <div className="lg:flex">
       <div className="lg:w-4/12 text-white text-center py-2 lg:py-0">
-        <h4 className="text-sm font-bold mb-2">Footer</h4>
-        <p className="text-xs">text</p>
+        <h4 className="text-sm font-bold mb-2">Additional Links</h4>
+        <p className="text-xs">Home</p>
+        <p className="text-xs">Anime</p>
+        <p className="text-xs">Ongoing</p>
       </div>
       <div className="lg:w-4/12 text-white text-center py-2">
-        <h4 className="text-sm font-bold mb-2">Anime</h4>
-        <p className="text-xs">text</p>
+        <h4 className="text-sm font-bold mb-2">Anime Categories</h4>
+        <p className="text-xs">Explore By Popularity</p>
+        <p className="text-xs">Explore By Random</p>
+        <p className="text-xs">Explore By Alphabetical Order</p>
+        <p className="text-xs">Explore By Type</p>
       </div>
       <div className="lg:w-4/12 text-white text-center py-2">
         <h4 className="text-sm font-bold mb-2">Account</h4>
