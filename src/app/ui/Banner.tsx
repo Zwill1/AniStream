2d1b0b18e8aca32b5bed7f59a0d5a77f158aac70
zwill <zwill_1@hotmail.com>
2024-02-03 18:57:26 -0700

    adding media queries on tailwind css block and hidden

--- src/app/ui/Banner.tsx
+++ src/app/ui/Banner.tsx
@@ -1,52 +1,55 @@
-"use client";
-
 import Image from "next/image";
 
 function BannerHero() {
     return (
       <>
-        <div className="md:w-6/12 text-center flex content-center">
-        <Image
-          src=""
-          alt="placeholder hero image"
-          className="dark:invert heroImage"
-          width={1900}
-          height={400}
-          priority
-        />
-        <style jsx>{`
-        /* displays anything past 1500px */
-          .heroImage {
-            background: red;
-          }
-        /* displays anything under 1500px */ 
-          @media (max-width: 1500px) {
-            .heroImage {
-              background: yellow;
-            }
-          }
-          @media (max-width: 1200px) {
-            .heroImage {
-              background: pink;
-            }
-          }
-          @media (max-width: 900px) {
-            .heroImage {
-              background: blue;
-            }
-          }
-          @media (max-width: 600px) {
-            .heroImage {
-              background: orange;
-            }
-          }
-        `}</style>
-        <style jsx>{`
-          body {
-            background: black;
-          }
-        `}</style>
-      </div>
+      {/* Small screens */}
+      <div className="sm:block md:hidden md:w-6/12 text-center flex content-center">
+      <Image
+        src="/images/hero-1900x400.png"
+        alt="placeholder hero image"
+        className="dark:invert heroImage"
+        width={1900}
+        height={400}
+        priority
+      />
+    </div>
+
+      {/* Medium screens */}
+    <div className="hidden md:block lg:hidden md:w-full text-center flex content-center">
+      <Image
+        src="/images/hero-1900x1200.png"
+        alt="placeholder hero image"
+        className="dark:invert heroImage"
+        width={1900}
+        height={400}
+        priority
+      />
+    </div>
+
+      {/* Large screens */}
+    <div className="hidden md:hidden lg:block xl:hidden lg:w-full text-center flex content-center">
+      <Image
+        src="/images/hero-1900x400.png"
+        alt="placeholder hero image"
+        className="dark:invert heroImage"
+        width={1900}
+        height={400}
+        priority
+      />
+    </div>
+
+      {/* XL screens */}
+    <div className="hidden md:hidden xl:block md:w-6/12 text-center flex content-center">
+      <Image
+        src="/images/hero-1900x1200.png"
+        alt="placeholder hero image"
+        className="dark:invert heroImage"
+        width={1900}
+        height={400}
+        priority
+      />
+    </div>
       </>
     )
   }
--- src/app/ui/Hero.tsx
+++ src/app/ui/Hero.tsx
@@ -1,21 +1,19 @@
+import Image from "next/image"
 import Button from "../components/Button"
 import BannerHero from "./Banner"
-import HelloWorld from "./Banner"
 
 export default function Hero() {
   return (
     <>
-    <section className="md:flex w-full flex-row-reverse">
-    {/* <div className="md:w-6/12 text-center flex content-center"> */}
-    <BannerHero />
-    {/* </div> */}
-    <div className="md:w-6/12 text-center px-10 py-20 bg-gray-800">
+    <section className="xl:flex w-full flex-row-reverse">
+      {/* top banner for media queries */}
+      <BannerHero />
+    <div className="w-full xl:w-6/12 text-center px-10 py-20 bg-gray-800">
       <h1 className="pb-2 text-white font-bold text-2xl uppercase">Welcome to AniStream</h1>
       <p className="text-white">Welcome to NextJS App – a haven crafted with love for anime enthusiasts like you! Immerse yourself in a captivating realm where your passion for Japanese animation is celebrated. Whether youre an avid otaku or just embarking on your anime journey, our community is your home. Explore our carefully curated library featuring timeless classics and the latest releases, connecting you with unforgettable characters and epic stories. Engage in lively discussions within our forums, swap recommendations, and uncover hidden gems.</p>
       <Button url={"/signin"}>Sign in NOW!</Button>
     </div>
     </section>
-    <HelloWorld />
     </>
   )
 }
